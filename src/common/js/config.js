d4da679ce3e31c373fc4bb8ec9ad84abca2ba36c
周旋 <zhouxuan@jkweixin.com>
2018-08-15 19:32:50 +0800

    修改详情查看时数据

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -9,11 +9,11 @@ const DEV_URL = {
 }
 
 const TEST_URL = {
-  api: 'http://agent-admin-api.net'
+  api: 'http://agent-admin-api.jkweixin.net'
 }
 
 const PROD_URL = {
-  api: 'http://agent-admin-api.com'
+  api: 'http://agent-admin-api.jkweixin.com'
 }
 
 export const BASE_URL = env.NODE_ENV === 'production' ? PROD_URL : env.NODE_ENV === 'test' ? TEST_URL : env.NODE_ENV === 'dev' ? DEV_URL : LOCAL_URL
